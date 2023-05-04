4db9ceea071034f51af231a1812373770716ad4f
garvsl <garvsehgal7@gmail.com>
2023-05-03 22:25:42 -0400

    add google oauth/signin/logout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import "./App.css";
+import Auth from "./components/auth";
 
 function App() {
-  return (
-    <div className="App">
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
-  );
+  return <Auth />;
 }
 
 export default App;
